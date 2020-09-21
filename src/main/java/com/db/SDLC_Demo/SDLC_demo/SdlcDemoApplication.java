c246bc2bc0e4f45cd15dab7d7728bc73d6a3a5b4
Sakshi <sakshisp7598@gmail.com>
2020-09-21 17:04:47 +0530

    Initial commit

---
+++ src/main/java/com/db/SDLC_Demo/SDLC_demo/SdlcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.db.SDLC_Demo.SDLC_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdlcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdlcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/db/SDLC_Demo/SDLC_demo/SdlcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.SDLC_Demo.SDLC_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdlcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
